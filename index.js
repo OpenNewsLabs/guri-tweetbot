261a8b42bb598c89b46ecade3897c66c8a49479e
Dan Zajdband <dan.zajdband@gmail.com>
2016-11-15 15:38:48 -0500

    trying show me command

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ stream.on('tweet',  tweet => {
   if(tweet.user.id_str === config.account_id) return;
   if(tweet.retweeted_status) return;
 
+	if (/show me /i.test(tweet.text)) {
+		const text = tweet.text.trim().replace('show me ', '')
+		return Promise.all([axios.get('/api/assets/search?type=panorama&query=${text}'), axios.get('/api/assets/search?type=audio&query=${text}')])
+		.then(([pano, audio]) => {
+			const body = []
+			if (pano && pano.length && (pano[0].url_k || pano[0].url_o)) {
+				body.push({ type: 'panorama', src: pano[0].url_k || pano[0].url_o  })
+			}
+
+			if (audio) {
+				body.push({ type: 'audio', src: audio })
+			}
+
+			if (!body.length) return T.post('statuses/update', {in_reply_to_status_id: tweet.id_str , status: `@${tweet.user.screen_name} your scene is here https://s3.amazonaws.com/gurivr/s/${res.data._id}.html` }, function(err, data, response) {});
+
+			axios.post('https://gurivr.com/api/stories', {
+				title: 'GuriVR - ' + text,
+				body: body
+			}).then(res => {
+				T.post('statuses/update', {in_reply_to_status_id: tweet.id_str , status: `@${tweet.user.screen_name} your scene is here https://s3.amazonaws.com/gurivr/s/${res.data._id}.html` }, function(err, data, response) {});
+			});
+	
+		});
+	}
+
   const media = tweet.entities.media
   .filter(m => m.type === 'photo');
 
